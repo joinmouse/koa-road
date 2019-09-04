f023577bd1c161f8f739a4b119fd1479c9dcea6d
joinmouse <908380434@qq.com>
2019-09-04 15:04:53 +0800

    开启一个简单的node 服务

---
+++ 01.js
@@ -0,0 +1,5 @@
+const Koa = require('koa')
+const app = new Koa()
+
+app.listen(3000)
+console.log('开启一个3000端口的服务, 就是这么简单的呀!!')
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # koa-road
+
+## node: 10.15.3
+
+## 1、安装koa: 架设一个简单的webserve
